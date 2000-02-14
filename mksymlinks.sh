0d8ae0cabef1a470f48ce4e71ea5b1749a59d049
curt <curt>
2000-02-14 17:37:54 +0000

    Restructuring subdirectories.

--- mksymlinks.sh
+++ mksymlinks.sh
@@ -4,22 +4,23 @@ echo ""
 echo "running $0 to rebuild simgear include links"
 
 # toast the old directory
-rm -rf simgear
-mkdir simgear
+rm -rf src/simgear
+mkdir src/simgear
 
 # build new links
-ln -s ../include/compiler.h simgear/compiler.h
-ln -s ../include/constants.h simgear/constants.h
-ln -s ../debug/debug_types.h simgear/debug_types.h
-ln -s ../math/fg_memory.h simgear/fg_memory.h
-ln -s ../include/fg_traits.hxx simgear/fg_traits.hxx
-ln -s ../math/fg_types.hxx simgear/fg_types.hxx
-ln -s ../include/fg_zlib.h simgear/fg_zlib.h
-ln -s ../misc/fgpath.hxx simgear/fgpath.hxx
-ln -s ../debug/logstream.hxx simgear/logstream.hxx
-ln -s ../math/mat3.h simgear/mat3.h
-ln -s ../bucket/newbucket.hxx simgear/newbucket.hxx
-ln -s ../math/point3d.hxx simgear/point3d.hxx
-ln -s ../math/polar3d.hxx simgear/polar3d.hxx
-ln -s ../xgl/xgl.h simgear/xgl.h
-ln -s ../misc/zfstream.hxx simgear/zfstream.hxx
+ln -s ../include/compiler.h src/simgear/compiler.h
+ln -s ../include/constants.h src/simgear/constants.h
+ln -s ../debug/debug_types.h src/simgear/debug_types.h
+ln -s ../math/fg_memory.h src/simgear/fg_memory.h
+ln -s ../include/fg_traits.hxx src/simgear/fg_traits.hxx
+ln -s ../math/fg_types.hxx src/simgear/fg_types.hxx
+ln -s ../include/fg_zlib.h src/simgear/fg_zlib.h
+ln -s ../misc/fgpath.hxx src/simgear/fgpath.hxx
+ln -s ../misc/fgstream.hxx src/simgear/fgstream.hxx
+ln -s ../debug/logstream.hxx src/simgear/logstream.hxx
+ln -s ../math/mat3.h src/simgear/mat3.h
+ln -s ../bucket/newbucket.hxx src/simgear/newbucket.hxx
+ln -s ../math/point3d.hxx src/simgear/point3d.hxx
+ln -s ../math/polar3d.hxx src/simgear/polar3d.hxx
+ln -s ../xgl/xgl.h src/simgear/xgl.h
+ln -s ../misc/zfstream.hxx src/simgear/zfstream.hxx

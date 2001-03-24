98a65b5d1dc5b74b23be27e9de614d8cb4130c95
curt <curt>
2001-03-24 04:21:37 +0000

    Doxygen ...

--- simgear/constants.h
+++ simgear/constants.h
@@ -52,75 +52,84 @@
 
 // SG_PI and SGD_PI (float and double) come from plib/sg.h
 
-// 2 * PI
+/** 2 * PI */
 #define SGD_2PI      6.28318530717958647692
 
-// PI / 2
+/** PI / 2 */
 #ifdef M_PI_2
 #  define  SGD_PI_2  M_PI_2
 #else
 #  define  SGD_PI_2  1.57079632679489661923
 #endif
 
-// PI / 4
+/** PI / 4 */
 #define SGD_PI_4     0.78539816339744830961
 
-#ifndef M_E
-#  define M_E     2.7182818284590452354
+/** \def SG_E "e" */
+#ifdef M_E
+#  define SG_E     M_E
+#else
+#  define SG_E     2.7182818284590452354
 #endif
 
-// SG_ONE_SECOND is pi/180/60/60, or about 100 feet at earths' equator
+/** pi/180/60/60, or about 100 feet at earths' equator */
 #define SG_ONE_SECOND 4.848136811E-6
 
 
-// Radius of Earth in kilometers at the equator.  Another source had
-// 6378.165 but this is probably close enough
+/** Radius of Earth in kilometers at the equator.  Another source had
+ *  6378.165 but this is probably close enough */
 #define SG_EARTH_RAD 6378.155
 
 
 // Earth parameters for WGS 84, taken from LaRCsim/ls_constants.h
 
-// Value of earth radius from [8]
-#define SG_EQUATORIAL_RADIUS_FT 20925650.    // ft
-#define SG_EQUATORIAL_RADIUS_M   6378138.12  // meter
-// Radius squared
-#define SG_EQ_RAD_SQUARE_FT 437882827922500.          // ft
-#define SG_EQ_RAD_SQUARE_M   40680645877797.1344      // meter
+/** Value of earth radius from LaRCsim (ft) */
+#define SG_EQUATORIAL_RADIUS_FT  20925650.
+
+/** Value of earth radius from LaRCsim (meter) */
+#define SG_EQUATORIAL_RADIUS_M   6378138.12
+
+/** Radius squared (ft) */
+#define SG_EQ_RAD_SQUARE_FT 437882827922500.
+
+/** Radius squared (meter) */
+#define SG_EQ_RAD_SQUARE_M   40680645877797.1344
 
 // Conversions
 
-// Arc seconds to radians                     // (arcsec*pi)/(3600*180) = rad
+/** Arc seconds to radians.  (arcsec*pi)/(3600*180) = rad */
 #define SG_ARCSEC_TO_RAD    4.84813681109535993589e-06 
 
-// Radians to arc seconds                     // (rad*3600*180)/pi = arcsec
+/** Radians to arc seconds.  (rad*3600*180)/pi = arcsec */
 #define SG_RAD_TO_ARCSEC    206264.806247096355156
 
-// Feet to Meters
+/** Feet to Meters */
 #define SG_FEET_TO_METER    0.3048
 
-// Meters to Feet
+/** Meters to Feet */
 #define SG_METER_TO_FEET    3.28083989501312335958  
 
-// Meters to Nautical Miles, 1 nm = 6076.11549 feet
+/** Meters to Nautical Miles.  1 nm = 6076.11549 feet */
 #define SG_METER_TO_NM      0.00053995680
 
-// Nautical Miles to Meters
+/** Nautical Miles to Meters */
 #define SG_NM_TO_METER      1852.0000
 
-// Radians to Nautical Miles, 1 nm = 1/60 of a degree
+/** Radians to Nautical Miles.  1 nm = 1/60 of a degree */
 #define SG_NM_TO_RAD        0.00029088820866572159
 
-// Nautical Miles to Radians
+/** Nautical Miles to Radians */
 #define SG_RAD_TO_NM        3437.7467707849392526
 
-// For divide by zero avoidance, this will be close enough to zero
+/** For divide by zero avoidance, this will be close enough to zero */
 #define SG_EPSILON 0.0000001
 
 
-// Highest binobj format version we know how to read/write.  This starts at
-// 0 and can go up to 65535
+/** Highest binobj format version we know how to read/write.  This starts at
+ *  0 and can go up to 65535 */
 #define SG_BINOBJ_VERSION 5
-// for backwards compatibility
+
+/** for backwards compatibility */
 #define SG_SCENERY_FILE_FORMAT "0.4"
 
 

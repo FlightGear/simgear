16dd841ce5cbdcd7b7a8bd1cfa4f99ab7d17c44e
curt <curt>
2003-05-09 20:19:36 +0000

    Moved loader.[ch]xx and model.[ch]xx from fgfs/src/Model/ to simgear/scene/model.

---
+++ simgear/scene/model/loader.hxx
@@ -0,0 +1,51 @@
+#ifndef __MODEL_LOADER_HXX
+#define __MODEL_LOADER_HXX 1
+
+#ifndef __cplusplus
+# error This library requires C++
+#endif
+
+#include <simgear/compiler.h>	// for SG_USING_STD
+
+#include <map>
+#include STL_STRING
+
+#include <plib/ssg.h>
+
+#include <simgear/props/props.hxx>
+
+SG_USING_STD(map);
+SG_USING_STD(string);
+
+
+/**
+ * Base class for loading and managing SSG things.
+ */
+class FGSSGLoader
+{
+public:
+    FGSSGLoader ();
+    virtual ~FGSSGLoader ();
+    virtual void flush ();
+protected:
+    std::map<string,ssgBase *> _table;
+};
+
+
+/**
+ * Class for loading and managing models with XML wrappers.
+ */
+class FGModelLoader : public FGSSGLoader
+{
+public:
+    FGModelLoader ();
+    virtual ~FGModelLoader ();
+
+    virtual ssgEntity *load_model( const string &fg_root,
+                                   const string &path,
+                                   SGPropertyNode *prop_root,
+                                   double sim_time_sec );
+};
+
+
+#endif

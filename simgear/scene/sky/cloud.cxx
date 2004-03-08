f6c9af488b51fa2a5aeb6fecb1206bd22386b728
ehofman <ehofman>
2004-03-08 08:59:58 +0000

    MacOS X refinement

--- simgear/scene/sky/cloud.cxx
+++ simgear/scene/sky/cloud.cxx
@@ -24,8 +24,8 @@
 // #if defined (__APPLE__) 
 // // any C++ header file undefines isinf and isnan
 // // so this should be included before <iostream>
-// inline int (isinf)(Real r) { return isinf(r); }
-// inline int (isnan)(Real r) { return isnan(r); }
+// inline int (isinf)(double r) { return isinf(r); }
+// inline int (isnan)(double r) { return isnan(r); } 
 // #endif
 
 // #include STL_IOSTREAM

80abe952ba99bfbec6e400e396d184b78c169733
ehofman <ehofman>
2005-10-15 14:59:02 +0000

    Ima Sudonim remembered me there is still one problem for gcc Vs. 3.4 or later under Cygwin. This fixes it.

--- simgear/screen/shader.cpp
+++ simgear/screen/shader.cpp
@@ -18,6 +18,9 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#ifdef HAVE_CONFIG_H
+#  include <simgear_config.h>
+#endif 
 
 #include <simgear/debug/logstream.hxx>
 #include "shader.h"

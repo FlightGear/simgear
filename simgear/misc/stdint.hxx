e994305da62e084139dfe7f426d2b5dc1675375b
ehofman <ehofman>
2005-11-13 09:42:38 +0000

    Put in public domain, Curtis wants it (because net_fdm.hxx depends on it) and I created the other functions.

--- simgear/misc/stdint.hxx
+++ simgear/misc/stdint.hxx
@@ -4,20 +4,10 @@
 
 // Copyright (C) 1999  Curtis L. Olson - http://www.flightgear.org/~curt
 //
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Library General Public
-// License as published by the Free Software Foundation; either
-// version 2 of the License, or (at your option) any later version.
+// Written by Curtis Olson - http://www.flightgear.org/~curt
+// Started September 2001.
 //
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Library General Public License for more details.
-//
-// You should have received a copy of the GNU Library General Public
-// License along with this library; if not, write to the
-// Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-// Boston, MA  02111-1307, USA.
+// This file is in the Public Domain, and comes with no warranty.
 //
 // $Id$
 

6b056e915e0c4c3a2cfe06c2283a3d4cf062b4b0
andy <andy>
2006-03-21 23:12:30 +0000

    Oops, implement the previously-mentioned fix without breaking support for omitting a length argument to mean "the rest of the vector".

--- simgear/nasal/lib.c
+++ simgear/nasal/lib.c
@@ -69,7 +69,8 @@ static naRef subvec(naContext c, naRef me, int argc, naRef* args)
         len = (int)nlen.num;
     if(!naIsVector(v) || start < 0 || start >= naVec_size(v) || len < 0)
         return naNil();
-    if(len > naVec_size(v) - start) len = naVec_size(v) - start;
+    if(naIsNil(nlen) || len > naVec_size(v) - start)
+        len = naVec_size(v) - start;
     result = naNewVector(c);
     naVec_setsize(result, len);
     for(i=0; i<len; i++)

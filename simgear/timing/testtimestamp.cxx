0dc9de81ae9d8fc64a4683ab82e9f1eca9398e1e
fredb <fredb>
2006-09-02 11:21:22 +0000

    Add a simple program to benchmark SGTimeStamp::stamp()

---
+++ simgear/timing/testtimestamp.cxx
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <simgear/timing/timestamp.hxx>
+
+using namespace std;
+
+int main()
+{
+    cout << "About to benchmark SGTimeStamp for 5 seconds. Press ENTER when ready." << endl;
+    int c = cin.get();
+    long nb = 0;
+    SGTimeStamp start, now;
+    start.stamp();
+    do {
+        nb += 1;
+        now.stamp();
+    } while ( ( now - start ) < 5000000 );
+
+    cout << ( nb / 5 ) << " iterations per seconds. Press ENTER to quit." << endl;
+    c = cin.get();
+}

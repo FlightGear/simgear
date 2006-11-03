5d3aacb892d28acb25bd668c39fb3336a8cc0cdd
fredb <fredb>
2006-11-03 18:08:46 +0000

    Attempt to fix the APIENTRY problem. It looks like a problem in OSG, or a conflict between OSG and plib/pui

--- simgear/math/SGMath.hxx
+++ simgear/math/SGMath.hxx
@@ -22,13 +22,10 @@
 
 #include <iosfwd>
 // FIXME, make it compile on IRIX
-#ifdef HAVE_CONFIG_H
-#  include <config.h>
-#endif
-#ifdef HAVE_WINDOWS_H
-#  include <windows.h>
-#endif
 #include <osg/GL>
+#undef GLUT_APIENTRY_DEFINED // GL/glut.h undef APIENTRY when this symbol is defined. osg/GL defines it (?).
+                             // This probably would work if we didn't use plib/pu.h that include GL/glut.h 
+                             //  on its side.
 
 #include "SGMathFwd.hxx"
 

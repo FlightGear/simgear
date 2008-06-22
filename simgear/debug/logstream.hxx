2d37c0aa426c3d2807f43e78777c21445282ba3a
mfranz <mfranz>
2008-06-22 18:07:50 +0000

    remove redundant #defines (they are already in compiler.h)

--- simgear/debug/logstream.hxx
+++ simgear/debug/logstream.hxx
@@ -309,9 +309,7 @@ sglog()
 # define SG_LOG(C,P,M) sglog() << loglevel(C,P) << M << std::endl
 #endif
 
-#define SG_STRINGIFY(x) #x
-#define SG_TOSTRING(x) SG_STRINGIFY(x)
-#define SG_ORIGIN __FILE__ ":" SG_TOSTRING(__LINE__)
+#define SG_ORIGIN __FILE__ ":" SG_STRINGIZE(__LINE__)
 
 #endif // _LOGSTREAM_H
 

d4c52b599dbd2b458709f05df2f7616a4025f59a
frohlich <frohlich>
2009-06-07 11:30:12 +0000

    Provide something more sensible for the properties root for the modelLoaded call. MAy be this needs to be revisited, but in any case better than constant zero.
    Modified Files:
     	simgear/scene/model/SGPagedLOD.cxx
    	simgear/scene/model/modellib.cxx

--- simgear/scene/model/SGPagedLOD.cxx
+++ simgear/scene/model/SGPagedLOD.cxx
@@ -56,7 +56,7 @@ bool SGPagedLOD::addChild(osg::Node *child)
     {
         osg::ref_ptr<SGModelData> d = opts->getModelData();
         if(d.valid())
-            d->modelLoaded(getFileName(getNumChildren()-1), 0,
+            d->modelLoaded(getFileName(getNumChildren()-1), opts->getPropRoot(),
                            this);
     }
     return true;

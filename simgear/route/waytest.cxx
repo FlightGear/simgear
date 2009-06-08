6e326976d55c6abf361456f90f49fb765a25a898
jmt <jmt>
2009-06-08 23:30:54 +0000

    Update route/waypoint tests for revised API

--- simgear/route/waytest.cxx
+++ simgear/route/waytest.cxx
@@ -43,24 +43,6 @@ int main() {
     cout << "Distance to " << b2.get_id() << " is " << distance * SG_METER_TO_NM
 	 << endl;
     cout << endl;
-
-    cur_lon = 10;
-    cur_lat = 10;
-
-    SGWayPoint c1(-20, 10, 0, SGWayPoint::CARTESIAN, "Due East");
-    c1.CourseAndDistance( cur_lon, cur_lat, cur_alt, &course, &distance );
-    cout << "Course to " << c1.get_id() << " is " << course << endl;
-    cout << "Distance to " << c1.get_id() << " is " << distance << endl;
-   
-    SGWayPoint c2(20, 20, 0, SGWayPoint::CARTESIAN, "Due SW");
-    c2.CourseAndDistance( cur_lon, cur_lat, cur_alt, &course, &distance );
-    cout << "Course to " << c2.get_id() << " is " << course << endl;
-    cout << "Distance to " << c2.get_id() << " is " << distance << endl;
-   
-    SGWayPoint c3(20, 0, 0, SGWayPoint::CARTESIAN, "Due NW");
-    c3.CourseAndDistance( cur_lon, cur_lat, cur_alt, &course, &distance );
-    cout << "Course to " << c3.get_id() << " is " << course << endl;
-    cout << "Distance to " << c3.get_id() << " is " << distance << endl;
    
     return 0;
 }

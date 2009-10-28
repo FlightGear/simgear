af14d65a9856119264f987cec44fa8c37c26d6c6
ehofman <ehofman>
2009-10-28 14:27:47 +0000

    Fix a bug where a sample was removed from the sample list before it was stopped. Proper listener orientation when inside the airplane (need to find a good solution for outside view).

--- simgear/sound/openal_test2.cxx
+++ simgear/sound/openal_test2.cxx
@@ -30,7 +30,7 @@ int main( int argc, char *argv[] ) {
     sample1->set_pitch(1.0);
     sample1->play_looped();
     sgr->add(sample1, "sound1");
-    smgr->update_late(1.0);
+    smgr->update(1.0);
     printf("playing sample1\n");
     sleep(1);
 
@@ -39,7 +39,7 @@ int main( int argc, char *argv[] ) {
     sample2->set_pitch(0.4);
     sample2->play_looped();
     sgr->add(sample2, "sound2");
-    smgr->update_late(1.0);
+    smgr->update(1.0);
     printf("playing sample2\n");
     sleep(1);
 
@@ -48,7 +48,7 @@ int main( int argc, char *argv[] ) {
     sample3->set_pitch(0.8);
     sample3->play_looped();
     sgr->add(sample3, "sound3");
-    smgr->update_late(1.0);
+    smgr->update(1.0);
     printf("playing sample3\n");
     sleep(1);
 
@@ -57,7 +57,7 @@ int main( int argc, char *argv[] ) {
     sample4->set_pitch(1.2);
     sample4->play_looped();
     sgr->add(sample4, "sound4");
-    smgr->update_late(1.0);
+    smgr->update(1.0);
     printf("playing sample4\n");
     sleep(1);
 
@@ -66,7 +66,7 @@ int main( int argc, char *argv[] ) {
     sample5->set_pitch(1.6);
     sample5->play_looped();
     sgr->add(sample5, "sound5");
-    smgr->update_late(1.0);
+    smgr->update(1.0);
     printf("playing sample5\n");
     sleep(1);
 
@@ -75,24 +75,24 @@ int main( int argc, char *argv[] ) {
     sample6->set_pitch(2.0);
     sample6->play_looped();
     sgr->add(sample6, "sound6");
-    smgr->update_late(1.0);
+    smgr->update(1.0);
     printf("playing sample6\n");
     sleep(1);
 
     for (int i=0; i<10; i++) {
         sleep(1);
-        smgr->update_late(1);
+        smgr->update(1);
     }
 
     sgr->stop("sound1");
     sgr->stop("sound2");
     sgr->stop("sound3");
     sleep(0.5);
-    smgr->update_late(0.5);
+    smgr->update(0.5);
     sgr->stop("sound4");
     sgr->stop("sound5");
     sgr->stop("sound6");
-    smgr->update_late(1);
+    smgr->update(1);
     sleep(1);
 
     smgr->unbind();

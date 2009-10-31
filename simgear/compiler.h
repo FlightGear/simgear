c8e60bcb595abe36a72b70faba70836685054ee7
ehofman <ehofman>
2009-10-31 08:46:46 +0000

    fix a gcc version test

--- simgear/compiler.h
+++ simgear/compiler.h
@@ -132,7 +132,7 @@
 
 #ifdef __APPLE__
 #  ifdef __GNUC__
-#    if ( __GNUC__ >= 3 ) && ( __GNUC_MINOR__ >= 3 )
+#    if ( __GNUC__ > 3 ) || ( __GNUC__ == 3 && __GNUC_MINOR__ >= 3 )
 inline int (isnan)(double r) { return !(r <= 0 || r >= 0); }
 #    else
     // any C++ header file undefines isinf and isnan

e7e0670708d96ccdfb6d257377807a6b42ebbb58
James Turner <zakalawe@mac.com>
2010-11-30 10:06:32 +0000

    SimGear CMake improvements.

--- CMakeModules/SimGearComponent.cmake
+++ CMakeModules/SimGearComponent.cmake
@@ -1,15 +1,20 @@
 
-macro(simgear_component name includePath sources header)
+macro(simgear_component name includePath sources headers)
 
     if (${SIMGEAR_SHARED})
         foreach(s ${sources})
             set_property(DIRECTORY "${PROJECT_SOURCE_DIR}/simgear" 
                 APPEND PROPERTY ALL_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/${s}")
         endforeach()
+
+		foreach(h ${headers})
+			set_property(DIRECTORY "${PROJECT_SOURCE_DIR}/simgear" 
+				APPEND PROPERTY PUBLIC_HEADERS "${CMAKE_CURRENT_SOURCE_DIR}/${h}")
+		endforeach()
         
     else()
         set(libName "sg${name}")
-        add_library(${libName} STATIC ${sources} ${headers} )
+        add_library(${libName} STATIC ${sources} )
 
         install (TARGETS ${libName} ARCHIVE DESTINATION lib)
         install (FILES ${headers}  DESTINATION include/simgear/${includePath})

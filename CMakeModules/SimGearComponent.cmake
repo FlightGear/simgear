2e2a59a603b0955ecf1ae3f4f48c320472235c3c
James Turner <zakalawe@mac.com>
2010-12-05 20:47:46 +0000

    Fix shared-library build of Simgear.

--- CMakeModules/SimGearComponent.cmake
+++ CMakeModules/SimGearComponent.cmake
@@ -3,12 +3,12 @@ macro(simgear_component name includePath sources headers)
 
     if (${SIMGEAR_SHARED})
         foreach(s ${sources})
-            set_property(DIRECTORY "${PROJECT_SOURCE_DIR}/simgear" 
+            set_property(GLOBAL
                 APPEND PROPERTY ALL_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/${s}")
         endforeach()
 
 		foreach(h ${headers})
-			set_property(DIRECTORY "${PROJECT_SOURCE_DIR}/simgear" 
+			set_property(GLOBAL
 				APPEND PROPERTY PUBLIC_HEADERS "${CMAKE_CURRENT_SOURCE_DIR}/${h}")
 		endforeach()
         

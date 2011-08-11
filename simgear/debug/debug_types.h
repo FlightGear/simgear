a668e47fa180ca6efb592a9406dcbe9909d69adf
ThorstenB <brehmt@gmail.com>
2011-08-11 23:45:19 +0200

    Introduce some more logging classes. The current habit of almost everything being in either SG_GENERAL or SG_ALL really sucks :).

--- simgear/debug/debug_types.h
+++ simgear/debug/debug_types.h
@@ -6,30 +6,33 @@
  * Define the possible classes/categories of logging messages
  */
 typedef enum {
-    SG_NONE      = 0x00000000,
+    SG_NONE        = 0x00000000,
 
-    SG_TERRAIN   = 0x00000001,
-    SG_ASTRO     = 0x00000002,
-    SG_FLIGHT    = 0x00000004,
-    SG_INPUT     = 0x00000008,
-    SG_GL        = 0x00000010,
-    SG_VIEW      = 0x00000020,
-    SG_COCKPIT   = 0x00000040,
-    SG_GENERAL   = 0x00000080,
-    SG_MATH      = 0x00000100,
-    SG_EVENT     = 0x00000200,
-    SG_AIRCRAFT  = 0x00000400,
-    SG_AUTOPILOT = 0x00000800,
-    SG_IO        = 0x00001000,
-    SG_CLIPPER   = 0x00002000,
-    SG_NETWORK   = 0x00004000,
-    SG_ATC       = 0x00008000,
-    SG_NASAL     = 0x00010000,
-    SG_INSTR     = 0x00020000,
-    SG_SYSTEMS   = 0x00040000,
-    SG_UNDEFD    = 0x00080000, // For range checking
+    SG_TERRAIN     = 0x00000001,
+    SG_ASTRO       = 0x00000002,
+    SG_FLIGHT      = 0x00000004,
+    SG_INPUT       = 0x00000008,
+    SG_GL          = 0x00000010,
+    SG_VIEW        = 0x00000020,
+    SG_COCKPIT     = 0x00000040,
+    SG_GENERAL     = 0x00000080,
+    SG_MATH        = 0x00000100,
+    SG_EVENT       = 0x00000200,
+    SG_AIRCRAFT    = 0x00000400,
+    SG_AUTOPILOT   = 0x00000800,
+    SG_IO          = 0x00001000,
+    SG_CLIPPER     = 0x00002000,
+    SG_NETWORK     = 0x00004000,
+    SG_ATC         = 0x00008000,
+    SG_NASAL       = 0x00010000,
+    SG_INSTR       = 0x00020000,
+    SG_SYSTEMS     = 0x00040000,
+    SG_AI          = 0x00080000,
+    SG_ENVIRONMENT = 0x00100000,
+    SG_SOUND       = 0x00200000,
+    SG_UNDEFD      = 0x00400000, // For range checkingng
 
-    SG_ALL     = 0xFFFFFFFF
+    SG_ALL         = 0xFFFFFFFF
 } sgDebugClass;
 
 

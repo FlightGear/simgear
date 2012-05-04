31ed79fe4311a4950a1574f7a6d42af434e072c6
ThorstenB <brehmt@gmail.com>
2012-05-05 00:30:16 +0200

    Fix rpmlint/Linux packager complaints about outdated/invalid FSF address in some license headers.

--- simgear/structure/SGSmplstat.cxx
+++ simgear/structure/SGSmplstat.cxx
@@ -13,7 +13,7 @@ implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 PURPOSE.  See the GNU Library General Public License for more details.
 You should have received a copy of the GNU Library General Public
 License along with this library; if not, write to the Free Software
-Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 #include <math.h>

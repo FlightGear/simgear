4993a5c6c04c32f62af801e03315ef481a6a1d47
Frederic Bouvier <fredfgfs01@free.fr>
2012-09-16 21:19:11 +0200

    Sort fgfs source files in IDE folders - Don't work for headers though

--- CMakeModules/SimGearComponent.cmake
+++ CMakeModules/SimGearComponent.cmake
@@ -1,14 +1,21 @@
 
 macro(simgear_component_common name includePath sourcesList sources headers)
+    set(fc${sourcesList} ${name})
+    set(fh${sourcesList} ${name})
     foreach(s ${sources})
         set_property(GLOBAL
             APPEND PROPERTY ${sourcesList} "${CMAKE_CURRENT_SOURCE_DIR}/${s}")
+        set(fc${sourcesList} "${fc${sourcesList}}#${CMAKE_CURRENT_SOURCE_DIR}/${s}")
     endforeach()
 
 	foreach(h ${headers})
 		set_property(GLOBAL
 			APPEND PROPERTY PUBLIC_HEADERS "${CMAKE_CURRENT_SOURCE_DIR}/${h}")
+        set(fh${sourcesList} "${fh${sourcesList}}#${CMAKE_CURRENT_SOURCE_DIR}/${h}")
 	endforeach()
+
+    set_property(GLOBAL APPEND PROPERTY FG_GROUPS_${sourcesList}_C "${fc${sourcesList}}@")
+    set_property(GLOBAL APPEND PROPERTY FG_GROUPS_${sourcesList}_H "${fh${sourcesList}}@")
     
     install (FILES ${headers}  DESTINATION include/simgear/${includePath})
 endmacro()

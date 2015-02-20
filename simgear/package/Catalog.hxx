611785c04b7da366656416ceb8bd3aa611e8a2a1
James Turner <zakalawe@mac.com>
2015-02-20 11:30:35 +0000

    Kill off allCatalogs list.
    No longer needed, and fixes crash on shutdown.

--- simgear/package/Catalog.hxx
+++ simgear/package/Catalog.hxx
@@ -32,9 +32,9 @@
 
 namespace simgear
 {
-    
+
 namespace HTTP { class Client; }
-    
+
 namespace pkg
 {
 
@@ -43,11 +43,11 @@ class Package;
 class Catalog;
 class Root;
 class Install;
-  
+
 typedef SGSharedPtr<Package> PackageRef;
 typedef SGSharedPtr<Catalog> CatalogRef;
 typedef SGSharedPtr<Install> InstallRef;
-  
+
 typedef std::vector<PackageRef> PackageList;
 typedef std::vector<CatalogRef> CatalogList;
 
@@ -55,16 +55,14 @@ class Catalog : public SGReferenced
 {
 public:
     virtual ~Catalog();
-    
+
     static CatalogRef createFromUrl(Root* aRoot, const std::string& aUrl);
-    
+
     static CatalogRef createFromPath(Root* aRoot, const SGPath& aPath);
-    
-    static CatalogList allCatalogs();
-    
+
     Root* root() const
         { return m_root;};
-    
+
     /**
      * perform a refresh of the catalog contents
      */
@@ -89,53 +87,53 @@ public:
     /**
      * retrieve all the packages in the catalog which are installed
      * and have a pendig update
-     */ 
+     */
     PackageList packagesNeedingUpdate() const;
-     
+
     SGPath installRoot() const
          { return m_installRoot; }
-    
+
     std::string id() const;
-    
+
     std::string url() const;
-    
+
     std::string description() const;
-    
+
     PackageRef getPackageById(const std::string& aId) const;
-  
+
     InstallRef installForPackage(PackageRef pkg) const;
-  
+
     /**
      * test if the catalog data was retrieved longer ago than the
      * maximum permitted age for this catalog.
      */
     bool needsRefresh() const;
-    
+
     unsigned int ageInSeconds() const;
-    
+
     /**
      * access the raw property data in the catalog
      */
     SGPropertyNode* properties() const;
 private:
     Catalog(Root* aRoot);
-    
+
     class Downloader;
     friend class Downloader;
-  
+
     friend class Install;
     void registerInstall(Install* ins);
     void unregisterInstall(Install* ins);
-  
+
     void parseProps(const SGPropertyNode* aProps);
-    
+
     void refreshComplete(Delegate::FailureCode aReason);
-    
+
     void parseTimestamp();
     void writeTimestamp();
-    
+
     std::string getLocalisedString(const SGPropertyNode* aRoot, const char* aName) const;
-    
+
     Root* m_root;
     SGPropertyNode_ptr m_props;
     SGPath m_installRoot;
@@ -146,13 +144,13 @@ private:
 
     typedef std::map<std::string, Package*> PackageWeakMap;
     PackageWeakMap m_variantDict;
-  
+
   // important that this is a weak-ref to Installs,
   // since it is only cleaned up in the Install destructor
     typedef std::map<PackageRef, Install*> PackageInstallDict;
     PackageInstallDict m_installed;
 };
-    
+
 } // of namespace pkg
 
 } // of namespace simgear

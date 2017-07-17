46f67fce7a643c118a68b9aa02a14ae4262f73ac
Ganael Laplanche <ganael.laplanche@martymac.org>
2017-07-17 14:49:51 +0200

    Remove dependency to TR1.
    TR1 will be deprecated in Boost 1.65, see:
    https://github.com/boostorg/admin/issues/123

--- simgear/scene/material/Effect.cxx
+++ simgear/scene/material/Effect.cxx
@@ -32,7 +32,7 @@
 #include <map>
 #include <queue>
 #include <utility>
-#include <boost/tr1/unordered_map.hpp>
+#include <unordered_map>
 
 #include <boost/bind.hpp>
 #include <boost/foreach.hpp>
@@ -832,13 +832,13 @@ size_t hash_value(const ProgramKey& key)
 
 // XXX Should these be protected by a mutex? Probably
 
-typedef tr1::unordered_map<ProgramKey, ref_ptr<Program>,
+typedef std::unordered_map<ProgramKey, ref_ptr<Program>,
                            boost::hash<ProgramKey>, ProgramKey::EqualTo>
 ProgramMap;
 ProgramMap programMap;
 ProgramMap resolvedProgramMap;  // map with resolved shader file names
 
-typedef tr1::unordered_map<ShaderKey, ref_ptr<Shader>, boost::hash<ShaderKey> >
+typedef std::unordered_map<ShaderKey, ref_ptr<Shader>, boost::hash<ShaderKey> >
 ShaderMap;
 ShaderMap shaderMap;
 
--- simgear/scene/material/Effect.hxx
+++ simgear/scene/material/Effect.hxx
@@ -19,7 +19,7 @@
 
 #include <vector>
 #include <string>
-#include <boost/tr1/unordered_map.hpp>
+#include <unordered_map>
 
 #include <boost/functional/hash.hpp>
 
@@ -127,7 +127,7 @@ protected:
             bool operator()(const Key& lhs, const Key& rhs) const;
         };
     };
-    typedef std::tr1::unordered_map<Key, osg::observer_ptr<Effect>,
+    typedef std::unordered_map<Key, osg::observer_ptr<Effect>,
                                     boost::hash<Key>, Key::EqualTo> Cache;
     Cache* getCache()
     {

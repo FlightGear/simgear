FlightGear/simgear

commit dfed2184f1e280281277030a8a94e3e0dd7dc326

SGEventMgr: Protection from timer insertion after shutdown.
This actually happens, Nasal timers are added after the event manager shutdown()
subsystem API call.

simgear/structure/event_mgr.cxx
@@ -10,6 +10,11 @@ void SGEventMgr::add(const std::string& name, SGCallback* cb,
                      double interval, double delay,
                      bool repeat, bool simtime)
 {
+    // Prevent Nasal from attempting to add timers after the subsystem has been
+    // shut down.
+    if (_shutdown)
+        return;
+
     // Clamp the delay value to 1 usec, so that user code can use
     // "zero" as a synonym for "next frame".
     if(delay <= 0) delay = 1e-6;
@@ -39,14 +44,15 @@ void SGTimer::run()
 }
 
 SGEventMgr::SGEventMgr() :
-    _inited(false)
+    _inited(false),
+    _shutdown(false)
 {
     
 }
 
 SGEventMgr::~SGEventMgr()
 {
-    
+    _shutdown = true;
 }
 
 void SGEventMgr::unbind()
@@ -69,7 +75,8 @@ void SGEventMgr::init()
 void SGEventMgr::shutdown()
 {
     _inited = false;
-    
+    _shutdown = true;
+
     _simQueue.clear();
     _rtQueue.clear();
 }

simgear/structure/event_mgr.hxx
@@ -130,7 +130,7 @@ private:
     SGPropertyNode_ptr _rtProp;
     SGTimerQueue _rtQueue; 
     SGTimerQueue _simQueue;
-    bool _inited;
+    bool _inited, _shutdown;
 };
 
 #endif // _SG_EVENT_MGR_HXX

commit 66cfa800beae582c4d447eeeec5276be9f18edbf

SGEventMgr: Reset the shutdown flag on init as a reset does not call the ctor.
This fixes the reset process that was broken by
dfed2184f1e280281277030a8a94e3e0dd7dc326.

simgear/structure/event_mgr.cxx
@@ -69,6 +69,9 @@ void SGEventMgr::init()
 		return;
     }
 	
+    // The event manager dtor and ctor are not called on reset, so reset the flag here.
+    _shutdown = false;
+
     _inited = true;
 }
 
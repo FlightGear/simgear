7853a5329d036e79fed954aea823532ccf0fe778
James Turner <zakalawe@mac.com>
2018-08-11 18:41:29 +0200

    Extend propsfwd.hxx so it can be used in more places

--- simgear/props/propsfwd.hxx
+++ simgear/props/propsfwd.hxx
@@ -1,3 +1,20 @@
+// Copyright (C) 2018  James Turner - <james@flightgear>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+
 /** \file
  *
  * Forward declarations for properties (and related structures)
@@ -7,11 +24,17 @@
 #define SG_PROPS_FWD_HXX
 
 #include <simgear/structure/SGSharedPtr.hxx>
+#include <vector>
 
 class SGPropertyNode;
 
 typedef SGSharedPtr<SGPropertyNode> SGPropertyNode_ptr;
 typedef SGSharedPtr<const SGPropertyNode> SGConstPropertyNode_ptr;
+
+namespace simgear
+{
+    using PropertyList = std::vector<SGPropertyNode_ptr>;
+}
 
 class SGCondition; 
 

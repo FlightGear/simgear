a53c1bfe8b3722113328d62ba20f06d6f0ea08f7
James Turner <zakalawe@mac.com>
2020-04-05 14:04:10 +0100

    CMake logic to export debug symbols

---
+++ CMakeModules/ExportDebugSymbols.cmake
@@ -0,0 +1,26 @@
+
+
+# placehodler target for other ones to depend upon
+add_custom_target(
+    debug_symbols
+)
+
+function(export_debug_symbols target)
+
+    if (NOT SIMGEAR_SHARED)
+        return()
+    endif()
+
+    if (APPLE)
+        add_custom_target(${target}.dSYM
+            COMMENT "Generating dSYM files for ${target}"
+            COMMAND dsymutil --out=${target}.dSYM $<TARGET_FILE:${target}>
+            DEPENDS $<TARGET_FILE:${target}>
+        ) 
+
+        install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${target}.dSYM DESTINATION symbols OPTIONAL)
+
+        add_dependencies(debug_symbols ${target}.dSYM)
+    endif()
+
+endfunction()

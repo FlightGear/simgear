69ad4a39e32e198c3a71600693020bc6eef9593a
Scott Giese <scttgs0@gmail.com>
2020-08-22 22:42:09 -0500

    REVIEW: Memory Leak - nasal newBlock()
    168 bytes in 7 blocks are still reachable

--- simgear/nasal/gc.c
+++ simgear/nasal/gc.c
@@ -301,6 +301,7 @@ static void newBlock(struct naPool* p, int need)
 
     if(need < MIN_BLOCK_SIZE) need = MIN_BLOCK_SIZE;
 
+    // REVIEW: Memory Leak - 168 bytes in 7 blocks are still reachable
     newb = naAlloc(sizeof(struct Block));
     // REVIEW: Memory Leak - 113,376 bytes in 1 blocks are still reachable
     newb->block = naAlloc(need * p->elemsz);

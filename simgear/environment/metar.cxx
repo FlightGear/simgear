152481bb13f74b742fcc3c17d7541f0262790b29
scttgs0 <scttgs0@gmail.com>
2023-05-08 20:05:49 -0500

    FLIGHTGEAR-1019 : metar temperature data malformed or missing
    DewPoint sensor failure not implemented.
    Expected: [M]99/[M]99
    Actual: 41///
    
    The actual value is conformant with the METAR specification, but is not implemented.
    
    Fix: implement handling of dew point sensor failure.  Assume a dew point value relative to the temperature in the event of sensor failure.

--- simgear/environment/metar.cxx
+++ simgear/environment/metar.cxx
@@ -1166,6 +1166,13 @@ bool SGMetar::scanTemperature()
 		_m += 5;
 		return scanBoundary(&_m);
 	}
+	if (!strncmp(m, "/////", 5)) {
+		// sensor failure... assume standard temperature
+		_temp = 15.0;
+		_dewp = 3.0;
+		_m += 5;
+		return scanBoundary(&_m);
+	}
 
 	if (*m == 'M')
 		m++, sign = -1;
@@ -1177,7 +1184,9 @@ bool SGMetar::scanTemperature()
 		return false;
 	if (!scanBoundary(&m)) {
 		if (!strncmp(m, "XX", 2))	                // not spec compliant!
-			m += 2, sign = 0, dew = temp;
+			m += 2, sign = 0, dew = temp - 10;
+		else if (!strncmp(m, "//", 2))	                // sensor failure... assume relative dew point
+			m += 2, sign = 0, dew = temp - 10;
 		else {
 			sign = 1;
 			if (*m == 'M')

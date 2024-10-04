3dee70fc6beadd357d337497b9f7f8300d8d1d38
Fernando García Liñán <fernando@fgarlin.com>
2024-10-04 16:04:10 +0200

    Add ignored messages to our OSG notify handler
    AMD cards report that texture units larger than 8 are not allowed, but
    they still work. Silence the warnings until we move to the core profile.
    
    Also move OsgIoCapture.hxx to scene/, because SimGearCore does not link
    against OSG.

--- simgear/scene/util/OsgIoCapture.hxx
+++ simgear/scene/util/OsgIoCapture.hxx
@@ -1,42 +1,54 @@
-#include <cstring>
+#include <algorithm>
 
 #include <osg/Notify>
 
 #include <simgear/debug/logstream.hxx>
+
+const std::string osg_ignored_messages[] = {
+    "0xde1",
+    "0x806f",
+};
 
 /**
 * merge OSG output into our logging system, so it gets recorded to file,
 * and so we can display a GUI console with renderer issues, especially
 * shader compilation warnings and errors.
 */
-class NotifyLogger : public osg::NotifyHandler
-{
+class SGNotifyHandler : public osg::NotifyHandler {
 public:
     // note this callback will be invoked by OSG from multiple threads.
     // fortunately our Simgear logging implementation already handles
     // that internally, so we simply pass the message on.
     virtual void notify(osg::NotifySeverity severity, const char* message) {
+        std::string msg(message);
+
+        // Remove the newline character, if any. SG_LOG already adds its own
+        if (msg.back() == '\n') {
+            msg.pop_back();
+        }
+
+        // Detect if this is an ignored message
+        auto it = std::find_if(std::begin(osg_ignored_messages),
+                               std::end(osg_ignored_messages),
+                               [&msg](const std::string& s) {
+                                   return msg.find(s) != std::string::npos;
+                               });
+        if (it != end(osg_ignored_messages)) {
+            return;
+        }
+
         // Detect whether a osg::Reference derived object is deleted with a non-zero
         // reference count. In this case trigger a segfault to get a stack trace.
-        if (strstr(message, "the final reference count was")) {
+        if (msg.find("the final reference count was") != std::string::npos) {
             // as this is going to segfault ignore the translation of severity and always output the message.
-            SG_LOG(SG_GL, SG_ALERT, message);
+            SG_LOG(SG_GL, SG_ALERT, msg);
 #ifndef DEBUG
-            throw std::string(message);
-            //int* trigger_segfault = 0;
-            //*trigger_segfault     = 0;
+            throw msg;
 #endif
             return;
         }
-        char*tmessage = strdup(message);
-        if (tmessage) {
-            char*lf = strrchr(tmessage, '\n');
-            if (lf)
-                *lf = 0;
 
-            SG_LOG(SG_OSG, translateSeverity(severity), tmessage);
-            free(tmessage);
-        }
+        SG_LOG(SG_OSG, translateSeverity(severity), msg);
     }
 
 private:

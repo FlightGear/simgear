1560c6236a70b032de377124f17e3d094fec2132
Fernando García Liñán <fernando@fgarlin.com>
2024-10-26 01:19:01 +0200

    GLcore: Remove fixed-pipeline state from VASI lights

--- simgear/scene/tgdb/pt_lights.cxx
+++ simgear/scene/tgdb/pt_lights.cxx
@@ -272,12 +272,6 @@ SGLightFactory::getLights(const SGDirectionalLightBin& lights)
   geometry->setNormalBinding(osg::Geometry::BIND_OFF);
   geometry->setColorArray(colors, osg::Array::BIND_PER_VERTEX);
 
-
-  //osg::StateSet* stateSet = geometry->getOrCreateStateSet();
-  //stateSet->setRenderBinDetails(POINT_LIGHTS_BIN, "DepthSortedBin");
-  //stateSet->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
-
-
   static SGSceneFeatures* sceneFeatures = SGSceneFeatures::instance();
   bool useTriangles = sceneFeatures->getEnableTriangleDirectionalLights();
 
@@ -358,18 +352,19 @@ SGLightFactory::getVasi(const SGVec3f& up, const SGDirectionalLightBin& lights,
   if (!drawable)
     return 0;
 
-  osg::StateSet* stateSet = drawable->getOrCreateStateSet();
-  stateSet->setRenderBinDetails(POINT_LIGHTS_BIN, "DepthSortedBin");
-  stateSet->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
+  // XXX: glcore: Should be handled by an Effect
+  // osg::StateSet* stateSet = drawable->getOrCreateStateSet();
+  // stateSet->setRenderBinDetails(POINT_LIGHTS_BIN, "DepthSortedBin");
+  // stateSet->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
 
-  osg::BlendFunc* blendFunc = new osg::BlendFunc;
-  stateSet->setAttribute(blendFunc);
-  stateSet->setMode(GL_BLEND, osg::StateAttribute::ON);
+  // osg::BlendFunc* blendFunc = new osg::BlendFunc;
+  // stateSet->setAttribute(blendFunc);
+  // stateSet->setMode(GL_BLEND, osg::StateAttribute::ON);
 
-  osg::AlphaFunc* alphaFunc;
-  alphaFunc = new osg::AlphaFunc(osg::AlphaFunc::GREATER, 0.01);
-  stateSet->setAttribute(alphaFunc);
-  stateSet->setMode(GL_ALPHA_TEST, osg::StateAttribute::ON);
+  // osg::AlphaFunc* alphaFunc;
+  // alphaFunc = new osg::AlphaFunc(osg::AlphaFunc::GREATER, 0.01);
+  // stateSet->setAttribute(alphaFunc);
+  // stateSet->setMode(GL_ALPHA_TEST, osg::StateAttribute::ON);
 
   return drawable;
 }

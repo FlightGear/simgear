811f060937dd0be2349bc21c6e88c41e3330195c
James Turner <james@flightgear.org>
2024-10-29 15:19:00 +0000

    Package: add SPDX headers

--- simgear/package/Package.hxx
+++ simgear/package/Package.hxx
@@ -1,22 +1,7 @@
-// Copyright (C) 2013  James Turner - zakalawe@mac.com
-//
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Library General Public
-// License as published by the Free Software Foundation; either
-// version 2 of the License, or (at your option) any later version.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Library General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// SPDX-FileCopyrightText: Copyright (C) 2013  James Turner - james@flightgear.org
 
-#ifndef SG_PACKAGE_PACKAGE_HXX
-#define SG_PACKAGE_PACKAGE_HXX
+#pragma once
 
 #include <set>
 #include <vector>
@@ -258,6 +243,4 @@ private:
 
 } // of namespace pkg
 
-} // of namespace simgear
-
-#endif // of SG_PACKAGE_PACKAGE_HXX
+} // namespace simgear

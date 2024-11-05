c78bef7bdf4fd8e71cf5ca71c3068dbf5bb5e127
Torsten Dreyer <torsten@t3r.de>
2024-11-05 21:03:58 +0000

    Use SG_LOG, add SPDX Header

--- simgear/io/DNSClient_cares.cxx
+++ simgear/io/DNSClient_cares.cxx
@@ -1,25 +1,5 @@
-/**
- * \file DNSClient.cxx - simple DNS resolver client engine for SimGear
- */
-
-// Written by James Turner
-//
-// Copyright (C) 2016 Torsten Dreyer - torsten (at) t3r (dot) de
-//
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Library General Public
-// License as published by the Free Software Foundation; either
-// version 2 of the License, or (at your option) any later version.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Library General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// SPDX-FileCopyrightText: Copyright (C) 2024  Torsten Dreyer - torsten@flightgear.org
 
 #include <simgear_config.h>
 
@@ -32,7 +12,7 @@
 #include <simgear/debug/logstream.hxx>
 #include <string.h>
 
-namespace simgear {
+    namespace simgear {
 
 namespace DNS {
 
@@ -86,7 +66,7 @@ public:
         Request* r = static_cast<Request*>(arg);
 
         if (status != ARES_SUCCESS) {
-            std::cerr << "DNS query failed: " << ares_strerror(status) << std::endl;
+            SG_LOG(SG_IO, SG_ALERT, "DNS query failed: " << ares_strerror(status) );
         } else {
             // this is not a very oo´ish approach
             switch( r->getType() ) {
@@ -100,7 +80,7 @@ public:
                     parse_TXT(abuf, alen, static_cast<TXTRequest*>(arg));
                     break;
                 default:
-                    std::cerr << "unhandled DNS callback for type: " << r->getType() << " ignored." << std::endl;
+                    SG_LOG(SG_IO, SG_WARN, "unhandled DNS callback for type: " << r->getType() << " ignored." );
                     break;
             }
         }
@@ -112,7 +92,7 @@ public:
         struct ares_naptr_reply* naptr_out;
         int result = ares_parse_naptr_reply(abuf, alen, &naptr_out);
         if (result != ARES_SUCCESS) {
-            std::cerr << "Failed to parse NAPTR reply: " << ares_strerror(result) << std::endl;
+            SG_LOG(SG_IO, SG_ALERT, "Failed to parse NAPTR reply: " << ares_strerror(result) );
             record->setComplete(true);
             return;
         }
@@ -149,7 +129,7 @@ public:
         struct ares_srv_reply* srv_out;
         int result = ares_parse_srv_reply(abuf, alen, &srv_out);
         if (result != ARES_SUCCESS) {
-            std::cerr << "Failed to parse SRV reply: " << ares_strerror(result) << std::endl;
+            SG_LOG(SG_IO, SG_ALERT, "Failed to parse SRV reply: " << ares_strerror(result) );
             record->setComplete(true);
             return;
         }
@@ -175,7 +155,7 @@ public:
         struct ares_txt_reply* txt_out;
         int result = ares_parse_txt_reply(abuf, alen, &txt_out);
         if (result != ARES_SUCCESS) {
-            std::cerr << "Failed to parse TXT reply: " << ares_strerror(result) << std::endl;
+            SG_LOG(SG_IO, SG_ALERT, "Failed to parse TXT reply: " << ares_strerror(result) );
             record->setComplete(true);
             return;
         }
--- simgear/io/test_DNS.cxx
+++ simgear/io/test_DNS.cxx
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// SPDX-FileCopyrightText: Copyright (C) 2024  Torsten Dreyer - torsten@flightgear.org
+
 #include <cstdlib>
 
 #include <atomic>

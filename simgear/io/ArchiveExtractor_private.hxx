bf0a2eedca38e01ec3e5741d39c672f4e49e9597
James Turner <james@flightgear.org>
2024-11-12 20:31:08 +0100

    Archive: add ‘remove top-most dir’ feature

--- simgear/io/ArchiveExtractor_private.hxx
+++ simgear/io/ArchiveExtractor_private.hxx
@@ -39,7 +39,6 @@ public:
     State state = INVALID;
     ArchiveExtractor* outer = nullptr;
     sha1nfo hashState;
-    bool doCreateDirHashes = false;
 
     virtual void extractBytes(const uint8_t* bytes, size_t count) = 0;
 
@@ -55,6 +54,15 @@ public:
         return outer->filterPath(pathToExtract);
     }
 
+    bool doRemoveTopmostDir() const
+    {
+        return outer->_removeTopmostDir;
+    }
+
+    bool doCreateDirHashes() const
+    {
+        return outer->_doCreateDirHashes;
+    }
 
     bool isSafePath(const std::string& p) const
     {

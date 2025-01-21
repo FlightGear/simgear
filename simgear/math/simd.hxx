87cea3eb3d1cf7dbf13734722d3207a6fa851176
James Turner <zakalawe@mac.com>
2025-01-21 15:18:39 +0000

    Really disable Neon for now

--- simgear/math/simd.hxx
+++ simgear/math/simd.hxx
@@ -28,15 +28,6 @@
 #include <cmath>
 #include <new>
 
-#if defined(_MSC_VER)
-# include <intrin.h>
-#elif defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__))
-# include <x86intrin.h>
-#elif defined(__GNUC__) && defined(__ARM_NEON__)
-# include <arm_neon.h>
-# include <simgear/math/simd_neon.hxx>
-#endif
-
 #include <simgear/math/SGLimits.hxx>
 #include <simgear/math/SGMisc.hxx>
 

c4033aecf516c40ba3cab8acc1afc273690455e8
Stuart Buchanan <stuart13@gmail.com>
2025-02-11 21:44:34 +0000

    GLTF Fixes
    - Support finding files in data locations.
    - Use the default Effect from the Options

--- simgear/scene/model/ReaderWriterGLTF.cxx
+++ simgear/scene/model/ReaderWriterGLTF.cxx
@@ -148,8 +148,11 @@ struct GLTFBuilder {
 
             // Read the material information from the glTF by creating an Effect
             SGPropertyNode_ptr effectRoot = new SGPropertyNode;
-            // Material is OPAQUE by default, so inherit from model-pbr
-            makeChild(effectRoot, "inherits-from")->setStringValue("Effects/model-pbr");
+            // Material is OPAQUE by default, so inherit from model-pbr unless something was set
+            // in the Options.
+            std::string effectName = "Effects/model-pbr";
+            if (opts->getDefaultEffect() != "") { effectName = opts->getDefaultEffect(); }
+            makeChild(effectRoot, "inherits-from")->setStringValue(effectName);
             if (primitive.material >= 0) {
                 // We have a material assigned to the primitive, add all the
                 // required material info as parameters to the Effect.

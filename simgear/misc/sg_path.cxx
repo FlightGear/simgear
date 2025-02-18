FlightGear/simgear

commit 81063d849db96b6772bf0dfb57bd93eab903fae5

SGPath: header cleanup

simgear/misc/sg_path.cxx
@@ -9,35 +9,32 @@
 
 // $Id$
 
-
-#include <simgear/compiler.h>
-
-#include <simgear_config.h>
-#include <simgear/debug/logstream.hxx>
-#include <simgear/misc/strutils.hxx>
-#include <simgear/io/iostreams/sgstream.hxx>
-
-#include <cstring>
-#include <stdio.h>
-#include <sys/stat.h>
-#include <errno.h>
-#include <fstream>
+#include <cerrno>
+#include <cstdio>
 #include <cstdlib>
+#include <cstring>
+#include <fstream>
+
+#include <sys/stat.h>
 
 #if !defined(SG_WINDOWS)
 #  include <sys/types.h>
 #  include <utime.h>
 #  include <unistd.h>
-#endif
-
-#if defined(SG_WINDOWS)
+#else
 #  include <direct.h>
 #  include <sys/utime.h>
 #  include <Shlwapi.h>
 #endif
 
-#include "sg_path.hxx"
+#include <simgear_config.h>
+#include <simgear/compiler.h>
+#include <simgear/debug/logstream.hxx>
+#include <simgear/io/iostreams/sgstream.hxx>
 #include <simgear/misc/sg_dir.hxx>
+#include <simgear/misc/strutils.hxx>
+
+#include "sg_path.hxx"
 
 using std::string;
 using simgear::strutils::starts_with;

commit c06c3d15b0813d0bb41d2153c1d9eee5af694ae9

Fix compilation on Windows (broken by commit 81063d849db96b)

simgear/misc/sg_path.cxx
@@ -9,6 +9,9 @@
 
 // $Id$
 
+#include <simgear/compiler.h>
+#include <simgear_config.h>
+
 #include <cerrno>
 #include <cstdio>
 #include <cstdlib>
@@ -28,8 +31,6 @@
 #  include <Shlwapi.h>
 #endif
 
-#include <simgear_config.h>
-#include <simgear/compiler.h>
 #include <simgear/debug/logstream.hxx>
 #include <simgear/io/iostreams/sgstream.hxx>
 #include <simgear/misc/sg_dir.hxx>